6742218fc8e2c2b5ce5baabb418d13bdf05db57f
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-15 00:47:28 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,17 +3,22 @@ require 'pry'
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
+  #   # binding.pry
   #     name: "Jon",
   #     email: "jon_snow@thewall.we", 
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
+  #   binding.pry
   #   "Freddy Mercury" => {
   #     name: "Freddy",
+  # binding.pry
   #     email: "freddy@mercury.com",
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.map do |key, value|
+    key.map do |key, fruit|
+      "@{fruit
 end
